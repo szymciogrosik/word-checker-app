674deb44417b861f885636c354a9921ce3dc6178
Szymon Gross <szyg@netcompany.com>
2026-03-09 17:19:02 +0100

    Added missing imports

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,13 @@ import {getAuth, provideAuth} from '@angular/fire/auth';
 import {getFirestore, provideFirestore} from '@angular/fire/firestore';
 import {getStorage, provideStorage} from '@angular/fire/storage';
 import {getAnalytics, provideAnalytics} from '@angular/fire/analytics';
+import {getFunctions, provideFunctions} from '@angular/fire/functions';
 import {provideTranslateService} from '@ngx-translate/core';
 import {AppComponent} from './app/app.component';
 import {routing} from './app/app-routing.module';
 import {environment} from './environments/environment';
-import {provideTranslateHttpLoader} from "@ngx-translate/http-loader";
-import {AssetsService} from "./app/_services/util/assets.service";
+import {provideTranslateHttpLoader} from '@ngx-translate/http-loader';
+import {AssetsService} from './app/_services/util/assets.service';
 import {MatNativeDateModule} from '@angular/material/core';
 import {MatPaginatorIntl} from '@angular/material/paginator';
 import {CustomPaginatorIntl} from './app/_services/util/custom-paginator-intl.service';
@@ -22,10 +23,11 @@ if (environment.production) {
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideZoneChangeDetection(), provideHttpClient(withInterceptorsFromDi()),
+    provideZoneChangeDetection(),
+    provideHttpClient(withInterceptorsFromDi()),
     provideTranslateService({
       loader: provideTranslateHttpLoader({
-        prefix: AssetsService.BASE_PATH + "i18n/",
+        prefix: AssetsService.BASE_PATH + 'i18n/',
         suffix: `.json?v=${environment.buildVersion}`
       }),
       fallbackLang: `${environment.default_language}`,
@@ -36,11 +38,12 @@ bootstrapApplication(AppComponent, {
 
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
+    provideFunctions(() => getFunctions()),
     provideFirestore(() => getFirestore()),
     provideStorage(() => getStorage()),
     provideAnalytics(() => getAnalytics()),
     {provide: MatPaginatorIntl, useClass: CustomPaginatorIntl}
   ]
 }).catch(error => {
-  console.error("Init failed: " + error)
+  console.error('Init failed: ' + error);
 });
